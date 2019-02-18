d5841b02f64cf03389c8f0517c4eb5988a46d87d
Ben Parisot <me@benparisot.com>
2019-02-18 10:17:48 -0800

    show location selection on location result/clue page

--- src/location.js
+++ src/location.js
@@ -1,6 +1,6 @@
 const locationHeader = document.getElementById('location');
 
 const searchParams = new URLSearchParams(window.location.search);
-const toFind = searchParams.get() 
+const locationName = searchParams.get('name');
 
-// locationHeader.textContent
+locationHeader.textContent = locationName;
