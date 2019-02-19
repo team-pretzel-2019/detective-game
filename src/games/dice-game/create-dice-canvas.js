f0b4cc38815d8ccdccbc343e068b33e9dcf55493
Ben Parisot <me@benparisot.com>
2019-02-19 13:43:18 -0800

    get crossword game working on location 2

---
+++ src/games/dice-game/create-dice-canvas.js
@@ -0,0 +1,88 @@
+import diceGame from './dice-game.js';
+import saveUser from '../../functions/saveUser.js';
+
+function createDiceCanvas(gameSection, currentLocation, gameOutcome, user) {
+    const playButton = document.createElement('button');
+    playButton.textContent = 'PLAY';
+    gameSection.appendChild(playButton);
+
+    const playerResultsSection = document.createElement('section');
+    playerResultsSection.id = 'player-results';
+    const playerDiceRolls = document.createElement('p');
+    playerDiceRolls.textContent = 'Your Dice Rolls: ';
+    const playerRoll1 = document.createElement('span');
+    playerRoll1.id = 'player-roll-1';
+    const playerRoll2 = document.createElement('span');
+    playerRoll2.id = 'player-roll-2';
+    playerDiceRolls.appendChild(playerRoll1);
+    playerDiceRolls.appendChild(playerRoll2);
+    gameSection.appendChild(playerDiceRolls);
+
+    const playerTotal = document.createElement('p');
+    playerTotal.textContent = 'Total: ';
+    const playerTotalResults = document.createElement('span');
+    playerTotalResults.id = 'player-total-results';
+    playerTotal.appendChild(playerTotalResults);
+    gameSection.appendChild(playerTotal);
+
+    const opponentResultsSection = document.createElement('section');
+    opponentResultsSection.id = 'opponent-results';
+    const opponentDiceRolls = document.createElement('p');
+    opponentDiceRolls.textContent = 'Opponent Dice Rolls: ';
+    const opponentRoll1 = document.createElement('span');
+    opponentRoll1.id = 'opponent-roll-1';
+    const opponentRoll2 = document.createElement('span');
+    opponentRoll2.id = 'opponent-roll-2';
+    opponentDiceRolls.appendChild(opponentRoll1);
+    opponentDiceRolls.appendChild(opponentRoll2);
+    gameSection.appendChild(opponentDiceRolls);
+
+    const opponentTotal = document.createElement('p');
+    opponentTotal.textContent = 'Total: ';
+    const opponentTotalResults = document.createElement('span');
+    opponentTotalResults.id = 'opponent-total-results';
+    opponentTotal.appendChild(opponentTotalResults);
+    gameSection.appendChild(opponentTotal);
+
+    const resultsMessageSection = document.createElement('section');
+    const resultMessage = document.createElement('p');
+    resultMessage.id = 'result-message';
+    resultsMessageSection.appendChild(resultMessage);
+    gameSection.appendChild(resultsMessageSection);
+
+    let gamesLost = 0;
+    
+    playButton.addEventListener('click', function() {
+        const gameResult = diceGame();
+        playerRoll1.textContent = gameResult.playerRoll1 + ', ';
+        playerRoll2.textContent = gameResult.playerRoll2;
+        playerTotalResults.textContent = gameResult.playerRoll1 + gameResult.playerRoll2;
+        opponentRoll1.textContent = gameResult.opponentRoll1 + ', ';
+        opponentRoll2.textContent = gameResult.opponentRoll2;
+        opponentTotalResults.textContent = gameResult.opponentRoll1 + gameResult.opponentRoll2;
+        resultMessage.textContent = gameResult.result;
+        if(gameResult.result === 'player wins') {
+            const clueP = document.createElement('p');
+            clueP.textContent = currentLocation.clue;
+            gameOutcome.appendChild(clueP);
+            user.receivedClues++;
+            user.daysLeft--;
+            saveUser(user);
+            gameSection.hidden = true;
+        } else {
+            gamesLost++;
+            console.log(gamesLost); 
+            
+        }
+        if(gamesLost === 3) {
+            gameSection.hidden = true;
+            const lossP = document.createElement('p');
+            lossP.textContent = 'You\'ve lost the game and lost a day, return to the map';
+            gameOutcome.appendChild(lossP);
+            user.daysLeft--;
+            saveUser(user);
+        }
+    });
+}
+
+export default createDiceCanvas;
--- src/location.js
+++ src/location.js
@@ -1,7 +1,8 @@
 import locationList from '../src/location-list.js';
 import saveUser from './functions/saveUser.js';
 import loadUser from './functions/loadUser.js';
-import diceGame from './games/dice-game/dice-game.js';
+import checkCrossword from './games/crossword/check-crossword.js';
+import createDiceCanvas from './games/dice-game/create-dice-canvas.js';
 
 const user = loadUser();
 
@@ -24,88 +25,42 @@ for(let i = 0; i < locationList.length; i++) {
 locationHeader.textContent = currentLocation.title;
 
 if(currentLocation.id === 'location1') {
-    const playButton = document.createElement('button');
-    playButton.textContent = 'PLAY';
-    gameSection.appendChild(playButton);
+    createDiceCanvas(gameSection, currentLocation, gameOutcome, user);
+}
+if(currentLocation.id === 'location2') {
+    // p for the promtp
+    const promptP = document.createElement('p');
+    promptP.textContent = 'I need a four letter word for flying that ends in an \'o\'.';
+    gameSection.appendChild(promptP);
+    // form
+    const crosswordForm = document.createElement('form');
+        //input
+    const guessInput = document.createElement('input');
+    guessInput.type = 'text';
+    guessInput.name = 'guess';
+    guessInput.id = 'guess-input';
+    guessInput.placeholder = 'Enter guess here';
+    crosswordForm.appendChild(guessInput);
+        //submit
+    const submitButton = document.createElement('button');
+    submitButton.textContent = 'Submit';
+    crosswordForm.appendChild(submitButton);
 
-    const playerResultsSection = document.createElement('section');
-    playerResultsSection.id = 'player-results';
-    const playerDiceRolls = document.createElement('p');
-    playerDiceRolls.textContent = 'Your Dice Rolls: ';
-    const playerRoll1 = document.createElement('span');
-    playerRoll1.id = 'player-roll-1';
-    const playerRoll2 = document.createElement('span');
-    playerRoll2.id = 'player-roll-2';
-    playerDiceRolls.appendChild(playerRoll1);
-    playerDiceRolls.appendChild(playerRoll2);
-    gameSection.appendChild(playerDiceRolls);
+    gameSection.appendChild(crosswordForm);
 
-    const playerTotal = document.createElement('p');
-    playerTotal.textContent = 'Total: ';
-    const playerTotalResults = document.createElement('span');
-    playerTotalResults.id = 'player-total-results';
-    playerTotal.appendChild(playerTotalResults);
-    gameSection.appendChild(playerTotal);
+    crosswordForm.addEventListener('submit', function(event) {
+        event.preventDefault();
 
-    const opponentResultsSection = document.createElement('section');
-    opponentResultsSection.id = 'opponent-results';
-    const opponentDiceRolls = document.createElement('p');
-    opponentDiceRolls.textContent = 'Opponent Dice Rolls: ';
-    const opponentRoll1 = document.createElement('span');
-    opponentRoll1.id = 'opponent-roll-1';
-    const opponentRoll2 = document.createElement('span');
-    opponentRoll2.id = 'opponent-roll-2';
-    opponentDiceRolls.appendChild(opponentRoll1);
-    opponentDiceRolls.appendChild(opponentRoll2);
-    gameSection.appendChild(opponentDiceRolls);
+        const formData = new FormData(crosswordForm);
+        const wordGuess = formData.get('guess');
+        const result = checkCrossword(wordGuess);
+        console.log(result);        
 
-    const opponentTotal = document.createElement('p');
-    opponentTotal.textContent = 'Total: ';
-    const opponentTotalResults = document.createElement('span');
-    opponentTotalResults.id = 'opponent-total-results';
-    opponentTotal.appendChild(opponentTotalResults);
-    gameSection.appendChild(opponentTotal);
-
-    const resultsMessageSection = document.createElement('section');
-    const resultMessage = document.createElement('p');
-    resultMessage.id = 'result-message';
-    resultsMessageSection.appendChild(resultMessage);
-    gameSection.appendChild(resultsMessageSection);
-
-    let gamesLost = 0;
-    
-    playButton.addEventListener('click', function() {
-        const gameResult = diceGame();
-        playerRoll1.textContent = gameResult.playerRoll1 + ', ';
-        playerRoll2.textContent = gameResult.playerRoll2;
-        playerTotalResults.textContent = gameResult.playerRoll1 + gameResult.playerRoll2;
-        opponentRoll1.textContent = gameResult.opponentRoll1 + ', ';
-        opponentRoll2.textContent = gameResult.opponentRoll2;
-        opponentTotalResults.textContent = gameResult.opponentRoll1 + gameResult.opponentRoll2;
-        resultMessage.textContent = gameResult.result;
-        if(gameResult.result === 'player wins') {
-            const clueP = document.createElement('p');
-            clueP.textContent = currentLocation.clue;
-            gameOutcome.appendChild(clueP);
-            user.receivedClues++;
-            user.daysLeft--;
-            saveUser(user);
-            gameSection.hidden = true;
-        } else {
-            gamesLost++;
-            console.log(gamesLost); 
-            
-        }
-        if(gamesLost === 3) {
-            gameSection.hidden = true;
-            const lossP = document.createElement('p');
-            lossP.textContent = 'You\'ve lost the game and lost a day, return to the map';
-            gameOutcome.appendChild(lossP);
-            user.daysLeft--;
-            saveUser(user);
-        }
     });
+    // wrong guess response
 
+    // game outcome
+        // win or lose message
 }
 
 saveUser(user);
