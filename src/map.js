cf977f6c3af574dd58ae3b84f336b918f9c4f347
Jared Myhrberg <jared.myhrberg@gmail.com>
2019-02-20 11:51:52 -0800

    user status on map working

--- src/map.js
+++ src/map.js
@@ -4,6 +4,43 @@ import loadUser from './functions/loadUser.js';
 
 const user = loadUser();
 
+const avatarImage = document.getElementById('avatar-image');
+const username = document.getElementById('username');
+const daysLeft = document.getElementById('days-left');
+const cluesList = document.getElementById('clues-list');
+
+const totalCluesListArray = [
+    'Cocktail napkin',
+    'Shave',
+    'Good view'
+];
+
+const currentCluesListArray = [];
+
+if(user.receivedClues >= 1) {
+    currentCluesListArray.push(totalCluesListArray[0]);
+}
+if(user.receivedClues >= 2) {
+    currentCluesListArray.push(totalCluesListArray[1]);
+}
+if(user.receivedClues >= 3) {
+    currentCluesListArray.push(totalCluesListArray[2]);
+}
+
+for(let i = 0; i < currentCluesListArray.length; i++) {
+    const clue = currentCluesListArray[i];
+    const clueLi = document.createElement('li');
+    clueLi.textContent = clue;
+    cluesList.appendChild(clueLi);
+}
+
+// avatarImage.src = ''; needs image
+username.textContent += user.name;
+daysLeft.textContent += user.daysLeft;
+
+
+
+
 const locationLinks = document.getElementById('location-links');
 
 for(let i = 0; i < locationList.length; i++) {
