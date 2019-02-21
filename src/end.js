f6bfec6c3e723c37256156cf339501f53cd29e16
Jared Myhrberg <jared.myhrberg@gmail.com>
2019-02-21 12:56:37 -0800

    game css

--- src/end.js
+++ src/end.js
@@ -4,9 +4,9 @@ const results = document.getElementById('results');
 const user = loadUser();
 
 if(user.daysLeft === 0) {
-    results.textContent = 'You\'re a big loser';
+    results.textContent = 'The trail\'s gone cold. You read in next week\'s newspaper that a woman named Violet is being tried for the murder of her parent. A man with a glass eye found Violet trying to dump her body in the river...';
 } else if(user.outcome === 'win') {
-    results.textContent = 'You have won the game.';
+    results.textContent = 'The glass eyed man lays on the ground at your feet. Police sirens wail as your head pounds. The police arrive and find Violet about to be buried, most likely after the killer dealt with you. The next day you get chewed out by the boys in blue, but your story checks and they let you go. You meet up with Violet who is a wreck, mentally and physically, but she\'ll recover. Case closed!';
 } else {
-    results.textContent = 'The villain killed ya';
+    results.textContent = 'The glass eyed man reaches you before you can get enough bullets in him. You two tussle but you\'re too weak from the blackjack you took to the head earlier. He knocks you out and when you wake up you are in a cold jail cell. The police buy his story and it looks like this is your new home...';
 }
