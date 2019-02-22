0f306a3d930892ccfc3273fbbbd9742712255817
Jared Myhrberg <jared.myhrberg@gmail.com>
2019-02-22 10:16:13 -0800

    rewrite to pass all tests

--- test/games/crossword/crossword.test.js
+++ test/games/crossword/crossword.test.js
@@ -4,7 +4,7 @@ const test = QUnit.test;
 QUnit.module('crossword testing');
 
 test('if input is aero return win message', function(assert) {
-    const guess = 'aero';
+    const guess = 'peso';
     const result = checkCrossword(guess);
     const expected = 'player wins';
 
@@ -21,7 +21,7 @@ test('if input is not aero return player loses', function(assert) {
 });
 
 test('if input is aero with weird captilzation, return player wins', function(assert) {
-    const guess = 'AerO';
+    const guess = 'pEsO';
     const result = checkCrossword(guess);
     const expected = 'player wins';
     
--- test/games/lock-door/lock-door.test.js
+++ test/games/lock-door/lock-door.test.js
@@ -4,10 +4,10 @@ const test = QUnit.test;
 QUnit.module('lock door tests');
 
 test('correct guess return player wins', function(assert) {
-    const guess1 = 1;
-    const guess2 = 2;
-    const guess3 = 3;
-    const guess4 = 4;
+    const guess1 = '2';
+    const guess2 = '3';
+    const guess3 = '5';
+    const guess4 = '8';
 
     const result = checkLock(guess1, guess2, guess3, guess4);
     const expected = 'player wins';
@@ -16,10 +16,10 @@ test('correct guess return player wins', function(assert) {
 });
 
 test('any wrong guess return player loses', function(assert) {
-    const guess1 = 1;
-    const guess2 = 2;
-    const guess3 = 5;
-    const guess4 = 4;
+    const guess1 = '1';
+    const guess2 = '2';
+    const guess3 = '5';
+    const guess4 = '4';
 
     const result = checkLock(guess1, guess2, guess3, guess4);
     const expected = 'player loses';
